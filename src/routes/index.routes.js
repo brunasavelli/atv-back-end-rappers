767a04b80fb1b0e3db6a245efd39098994130525
Bruna Savelli <bnsavelli@gmail.com>
2024-10-10 09:37:16 -0300

    feat: create routes configs

---
+++ src/routes/index.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+import rappersRoutes from "./rappers.routes.js";
+
+const routes = Router();
+
+routes.get("/", (req, res) => {
+    return res.status(200).send({ message: "Servidor funcionando!" })
+})
+
+routes.use("/rappers", rappersRoutes);
+
+export default routes;
---
+++ src/routes/rappers.routes.js
@@ -0,0 +1,43 @@
+import { Router } from "express"
+
+const rappersRoutes = Router()
+
+let suspeitos = [
+    {
+        id: Number(Math.floor(Math.random() * 999) + 1),
+        nome_suspeito: "P-Diddy",
+        idade: 54,
+        atividade_suspeita: true,
+        descricao_fisica: [
+            "Homem",
+            "Negro",
+            "Barbudo",
+            "1,78m de altura"
+        ]
+    },
+    {
+        id: Number(Math.floor(Math.random() * 999) + 1),
+        nome_suspeito: "Justin Bieber",
+        idade: 30,
+        atividade_suspeita: false,
+        descricao_fisica: [
+            "Homem",
+            "Loiro",
+            "Tatuado",
+            "1,75m de altura"
+        ]
+    },
+    {
+        id: Number(Math.floor(Math.random() * 999) + 1),
+        nome_suspeito: "Eminem",
+        idade: 50,
+        atividade_suspeita: false,
+        descricao_fisica: [
+            "Homem",
+            "Branco",
+            "Barbudo",
+            "1,73m de altura"
+        ]
+    }
+]
+
