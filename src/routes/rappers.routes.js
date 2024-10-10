4475f17a94631b91f59e43f41888f50a8362b955
Bruna Savelli <bnsavelli@gmail.com>
2024-10-10 10:52:02 -0300

    feat: create post method

--- src/routes/rappers.routes.js
+++ src/routes/rappers.routes.js
@@ -7,7 +7,7 @@ let suspeitos = [
         id: Number(Math.floor(Math.random() * 999) + 1),
         nome_suspeito: "P-Diddy",
         idade: 54,
-        atividade_suspeita: true,
+        atividade_suspeita: "sim",
         descricao_fisica: [
             "Homem",
             "Negro",
@@ -19,7 +19,7 @@ let suspeitos = [
         id: Number(Math.floor(Math.random() * 999) + 1),
         nome_suspeito: "Justin Bieber",
         idade: 30,
-        atividade_suspeita: false,
+        atividade_suspeita: "não",
         descricao_fisica: [
             "Homem",
             "Loiro",
@@ -31,7 +31,7 @@ let suspeitos = [
         id: Number(Math.floor(Math.random() * 999) + 1),
         nome_suspeito: "Eminem",
         idade: 50,
-        atividade_suspeita: false,
+        atividade_suspeita: "não",
         descricao_fisica: [
             "Homem",
             "Branco",
@@ -46,6 +46,38 @@ rappersRoutes.get("/", (req,res) => {
     return res.status(200).send(suspeitos)
 })
 
+//Cadastrar um novo suspeito
+rappersRoutes.post("/", (req,res) => {
+    const {nome_suspeito, idade, atividade_suspeita, descricao_fisica} = req.body
 
+    const novoSuspeito = {
+        id: Number(Math.floor(Math.random() * 999) + 1),
+        nome_suspeito,
+        idade,
+        atividade_suspeita,
+        descricao_fisica
+    };
+
+    //Nome obrigatório
+    if (!nome_suspeito) {
+        return res.status(400).send({
+            message: "O nome do suspeito é obrigatório!"
+        })
+    }
+
+    //Idade úmero inteiro
+    const validacaoIdade = Number.isInteger(idade);
+    console.log(validacaoIdade);
+
+    //Validação de atividade suspeita
+    if (atividade_suspeita != "sim" && atividade_suspeita != "não") {
+        return res.status(400).send({
+            message: "Digite sim ou não para atividades suspeitas!"
+        })
+    }
+
+    suspeitos.push(novoSuspeito);
+    return res.status(201).send(suspeitos)
+})
 
 export default rappersRoutes
